eae9fe133926a8d3d4702111fb133035d3c208b5
PyrotechnicCake <PyrotechnicCake@gmail.com>
2021-06-21 16:55:37 +1000

    input script changes

--- Souls of the Emblem/Assets/Scripts/InputHandler.cs
+++ Souls of the Emblem/Assets/Scripts/InputHandler.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class InputHandler : MonoBehaviour
+namespace Pyro
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InputHandler : MonoBehaviour
     {
-        
-    }
+        //Initialize input values
+        public float horizontal;
+        public float vertical;
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //track inputs
+        PlayerControls inputActions;
+
+        Vector2 movementInput;
+        Vector2 cameraInput;
+
+        //start reciving inputs
+        public void OnEnable()
+        {
+            if (inputActions == null) //if no input actions are found, get them
+            {
+                inputActions = new PlayerControls();
+                inputActions.PlayerMovement.Movement.performed += inputActions => movementInput = inputActions.ReadValue<Vector2>(); //must be "inputActions" not "inputAction"
+                inputActions.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
+            }
+
+            inputActions.Enable();
+        }
+
+        //stop recieving inputs
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+        public void TickInput(float delta)
+        {
+            MoveInput(delta);
+        }
+
+        //take movement inputs
+        private void MoveInput(float delta)
+        {
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+            mouseX = cameraInput.x;
+            mouseY = cameraInput.y;
+        }
     }
 }
--- Souls of the Emblem/Assets/Scripts/PlayerLocomotion.cs
+++ Souls of the Emblem/Assets/Scripts/PlayerLocomotion.cs
@@ -2,17 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLocomotion : MonoBehaviour
+namespace Pyro
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerLocomotion : MonoBehaviour
     {
-        
-    }
+        //initialize objects and vectors
+        Transform cameraObject;
+        InputHandler inputHandler;
+        Vector3 moveDirection;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //track position
+        [HideInInspector]
+        public Transform myTransform;
+
+        //initialize rigidbody and camera
+        public new Rigidbody rigidbody;
+        public GameObject normalCamera;
+
+        [Header("Stats")]
+        [SerializeField]
+        float movementSpeed = 5;
+        [SerializeField]
+        float rotationSpeed = 10;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //get rigidbody, camera and input handler
+            rigidbody = GetComponent<Rigidbody>();
+            inputHandler = GetComponent<InputHandler>();
+            cameraObject = Camera.main.transform;
+            myTransform = transform;
+
+        }
+
+        #region Movement
+        Vector3 normalVector;
+        Vector3 targetPosition;
+
+        private void HandleRotation(float delta)
+        {
+            Vector3 targetDir = Vector3.zero;
+            float moveOverride = inputHandler.moveAmount;
+
+            targetDir = cameraObject.forward * inputHandler.vertical;
+            targetDir += cameraObject.right * inputHandler.horizontal;
+
+            targetDir.Normalize();
+            targetDir.y = 0;
+
+            if (targetDir == Vector3.zero)
+                targetDir = myTransform.forward;
+
+            float rs = rotationSpeed;
+
+            Quaternion tr = Quaternion.LookRotation(targetDir);
+            Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta);
+
+            myTransform.rotation = targetRotation;
+        }
+        #endregion
     }
 }
